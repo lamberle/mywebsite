79d5145d27d0eaa825ba8cab08eff574e97efb0c
Léo <leo-lambert@hotmail.fr>
2020-11-01 18:26:38 +0100

    about page rework

--- view/site/index.php
+++ view/site/index.php
@@ -1,20 +1,50 @@
-<div class="item">
-    <div class="polaroid"><img src="ressources/IMG_2137_2.jpg">
-      <div class="caption">I Miss London</div>
-    </div>
-  </div>
+<div class="about-section">
+	<center>
+	<div>
+		<div class="item"  style="filter: grayscale(0%)">
+			<div class="polaroid"><img src="ressources/IMG_0984.jpg">
+			  <div class="caption" style="opacity: 100%;">Hello, I'm Léo LAMBERT</div>
+			</div>
+		</div>
+		<div class="place-holder"></div>
+	</div>
+</center>
+<center>
+	<div>
+		<div class="place-holder"></div>
+		<div class="item">
+			<div class="polaroid"><img src="ressources/IMG_2137_2.jpg">
+			  <div class="caption">Hi was born and raised in Savoie, France</div>
+			</div>
+		</div>
+	</div>
+</center>
+</div>
+<div class="about-section following-section">
+	<center>
+	<div>
+		<div class="item">
+			<div class="polaroid"><img src="ressources/paris.jpg">
+			  <div class="caption">But I'm currently working in Paris as a Data Engineer</div>
+			</div>
+		</div>
+		<div class="place-holder"></div>
+	</div>
+</center>
+<center>
+	<div>
+		<div class="place-holder"></div>
+		<div class="item">
+			<div class="polaroid"><img src="ressources/IMG_2085.jpg">
+			  <div class="caption">Photography</div>
+			</div>
+		</div>
+	</div>
+</center>
+</div>
 
-  <div class="item">
-    <div class="polaroid"><img src="ressources/IMG_0984.jpg">
-      <div class="caption">Léo LAMBERT</div>
-    </div>
-  </div>
 
-  <div class="item">
-    <div class="polaroid"><img src="ressources/IMG_1380.jpg">
-      <div class="caption">Summer Day</div>
-    </div>
-  </div>
+
 
 
 <?php
